22063af70367107f6c22e22f100d397ce5b85940
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-31 06:44:06 +0000

    Modification du composant principal app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArticleListComponent, HttpClientModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'gestion-articles';
+  
 }
+
