5ff501e59e450f2ed6f2e482ed524d4d2e3b2312
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-31 06:51:16 +0000

    Mise en place du component pour les details d'un article

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleService } from '../article.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-article-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, HttpClientModule],
   templateUrl: './article-detail.component.html',
-  styleUrl: './article-detail.component.css'
+  styleUrls: ['./article-detail.component.css']
 })
-export class ArticleDetailComponent {
+export class ArticleDetailComponent implements OnInit {
+  article: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.articleService.getArticle(id).subscribe(article => {
+        this.article = article;
+      });
+    });
+  }
+
+  deleteArticle(id: number): void {
+    this.articleService.deleteArticle(id).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
+
+  editArticle(id: number): void {
+    this.router.navigate(['/article', id, 'edit']);
+  }
 }
