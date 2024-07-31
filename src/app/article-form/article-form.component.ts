1db2ed7a1af7a87f7755991b572ba8bd52c16a2b
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-31 06:42:34 +0000

    Générer les trois components

---
+++ src/app/article-form/article-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-form.component.html',
+  styleUrl: './article-form.component.css'
+})
+export class ArticleFormComponent {
+
+}
