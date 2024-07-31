f7234097b2384952b45398c97db1fee11c405424
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-31 06:41:11 +0000

    réglages des routes et les providers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { ArticleFormComponent } from './article-form/article-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ArticleListComponent },
+  { path: 'article/new', component: ArticleFormComponent },
+  { path: 'article/:id', component: ArticleDetailComponent },
+  { path: 'article/:id/edit', component: ArticleFormComponent }
+];
