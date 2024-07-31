b97b0e27b7beddae6a1cd6140d0460f8e6702715
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-31 13:32:58 +0000

    refactoring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { ArticleListComponent } from './article-list/article-list.component';
-// import { ArticleFormComponent } from './article-form/article-form.component';
-// import { ArticleDetailComponent } from './article-detail/article-detail.component';
-
-
-// export const routes: Routes = [
-//   { path: '', component: ArticleListComponent },
-//   { path: 'article/new', component: ArticleFormComponent },
-//   { path: 'article/edit/:id', component: ArticleFormComponent },
-//   { path: 'article/:id', component: ArticleDetailComponent },
-// ];
-
 import { Routes } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,37 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { Article } from './article.module';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ArticleService {
-//   private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
-
-//   constructor(private http: HttpClient) { }
-
-//   getArticles(): Observable<Article[]> {
-//     return this.http.get<Article[]>(this.apiUrl);
-//   }
-
-//   getArticle(id: number): Observable<Article> {
-//     return this.http.get<Article>(`${this.apiUrl}/${id}`);
-//   }
-
-//   createArticle(article: Article): Observable<Article> {
-//     return this.http.post<Article>(this.apiUrl, article);
-//   }
-
-//   updateArticle(id: number, article: Article): Observable<Article> {
-//     return this.http.put<Article>(`${this.apiUrl}/${id}`, article);
-//   }
-
-//   deleteArticle(id: number): Observable<void> {
-//     return this.http.delete<void>(`${this.apiUrl}/${id}`);
-//   }
-// }
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
